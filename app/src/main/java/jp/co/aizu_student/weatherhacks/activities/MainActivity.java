b6bfb85748be63a171fc71ac1d71a6f5402bac56
Kento Kobayashi <kobayashi_kento@nomadinc.co.jp>
2015-05-28 20:41:13 +0900

    make best practice package

--- app/src/main/java/jp/co/aizu_student/weatherhacks/activities/MainActivity.java
+++ app/src/main/java/jp/co/aizu_student/weatherhacks/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package jp.co.aizu_student.weatherhacks;
+package jp.co.aizu_student.weatherhacks.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import jp.co.aizu_student.weatherhacks.R;
 
 
 public class MainActivity extends ActionBarActivity {
