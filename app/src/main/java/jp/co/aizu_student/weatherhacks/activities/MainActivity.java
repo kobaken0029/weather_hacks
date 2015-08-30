1d43ec35d3cab30b1fe900b35ac086e576f6d8bc
Kento Kobayashi <kobayashi_kento@nomadinc.co.jp>
2015-08-30 18:56:02 +0900

    remove menu

--- app/src/main/java/jp/co/aizu_student/weatherhacks/activities/MainActivity.java
+++ app/src/main/java/jp/co/aizu_student/weatherhacks/activities/MainActivity.java
@@ -9,25 +9,9 @@ import jp.co.aizu_student.weatherhacks.R;
 
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
