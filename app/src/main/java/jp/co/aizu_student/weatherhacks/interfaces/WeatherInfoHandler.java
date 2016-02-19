af3529b43c9935d57dc26ab9390b2bb1d8976d22
kobayashi_kento <kobaken0029@gmail.com>
2016-02-19 10:23:43 +0900

    不要import削除

--- app/src/main/java/jp/co/aizu_student/weatherhacks/interfaces/WeatherInfoHandler.java
+++ app/src/main/java/jp/co/aizu_student/weatherhacks/interfaces/WeatherInfoHandler.java
@@ -1,7 +1,5 @@
 package jp.co.aizu_student.weatherhacks.interfaces;
 
-import android.view.View;
-
 import jp.co.aizu_student.weatherhacks.models.WeatherInfo;
 
 public interface WeatherInfoHandler {
