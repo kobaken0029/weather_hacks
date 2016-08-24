3b8ddc1f2709afc3e1bd3d77af436a3e7915f5d7
kobayashi_kento <kobaken0029@gmail.com>
2016-08-25 02:23:16 +0900

    スワイプリフレッシュの削除

--- app/src/main/java/jp/co/aizu_student/weatherhacks/views/activities/MainActivity.java
+++ app/src/main/java/jp/co/aizu_student/weatherhacks/views/activities/MainActivity.java
@@ -5,10 +5,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.Toolbar;
 
 import javax.inject.Inject;
@@ -21,7 +19,7 @@ import jp.co.aizu_student.weatherhacks.models.Location;
 import jp.co.aizu_student.weatherhacks.adapter.MyPagerAdapter;
 
 public class MainActivity extends BaseActivity
-        implements ViewPager.OnPageChangeListener, SwipeRefreshLayout.OnRefreshListener {
+        implements ViewPager.OnPageChangeListener {
 
     /** リクエストコード */
     public static final int REQUEST_CODE = 1;
@@ -98,7 +96,6 @@ public class MainActivity extends BaseActivity
         binding.tabLayout.setupWithViewPager(binding.viewPager);
         binding.tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         binding.tabLayout.setTabMode(TabLayout.MODE_FIXED);
-        binding.setRefreshListener(this);
     }
 
     @Override
@@ -111,16 +108,5 @@ public class MainActivity extends BaseActivity
 
     @Override
     public void onPageSelected(int position) {
-    }
-
-    @Override
-    public void onRefresh() {
-        new Handler().postDelayed(() -> {
-            weatherHacksApiHelper.requestWeather(
-                    WeatherHacks.getInstance().getLocationId(),
-                    getSupportFragmentManager()
-            );
-            binding.swipeRefreshLayout.setRefreshing(false);
-        }, 1500);
     }
 }
