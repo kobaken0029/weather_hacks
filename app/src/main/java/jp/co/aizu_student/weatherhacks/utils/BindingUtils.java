c099d5d1037a75a9c35cbca455abdaeb9a745d2c
kobayashi_kento <kobaken0029@gmail.com>
2016-09-15 14:49:27 +0900

    Removed unused binding method

--- app/src/main/java/jp/co/aizu_student/weatherhacks/utils/BindingUtils.java
+++ app/src/main/java/jp/co/aizu_student/weatherhacks/utils/BindingUtils.java
@@ -2,12 +2,10 @@ package jp.co.aizu_student.weatherhacks.utils;
 
 import android.databinding.BindingMethod;
 import android.databinding.BindingMethods;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.widget.ListView;
 
 @BindingMethods({
         @BindingMethod(type = ListView.class, attribute = "android:onItemClick", method = "setOnItemClickListener"),
-        @BindingMethod(type = SwipeRefreshLayout.class, attribute = "android:onSwipeRefresh", method = "setOnRefreshListener")
 })
 public final class BindingUtils {
 }
